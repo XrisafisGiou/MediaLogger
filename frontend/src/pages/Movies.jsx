d2220b7bb84ef911bc9d8b3c10bc2b47f035b4b5
Chrysafis Gkiouzelis <114904304+XrisafisGiou@users.noreply.github.com.>
2026-07-02 20:56:53 +0300

    Improved movies page

--- frontend/src/pages/Movies.jsx
+++ frontend/src/pages/Movies.jsx
@@ -6,93 +6,186 @@ import {
   deleteMovie,
   searchMovies,
 } from "../services/api.js";
+import { Eye, Heart, Trash2, LogOut, Bookmark } from "lucide-react";
 
-import { Eye, Heart, Trash2 } from "lucide-react";
-
-const iconSize = 25;
+const iconSize = 30;
 
 export default function Movies() {
   const [movies, setMovies] = useState([]);
-  const [searchQuery, setSearchQuery] = useState("");
+  const [searchQuery, setSearchQuery] = useState(""); 
   const [searchResults, setSearchResults] = useState([]);
+  const [activeTab, setActiveTab] = useState("watched");
+  const [isSearchOpen, setIsSearchOpen] = useState(false);
 
   async function loadMovies() {
     const data = await getMovies();
     setMovies(data);
   }
 
+  function handleLogout() {
+  localStorage.removeItem("token");
+  window.location.href = "/"
+  }
+
   useEffect(() => {
     loadMovies();
   }, []);
 
-  async function handleSearch(e) {
-    e.preventDefault();
-    const data = await searchMovies(searchQuery);
-    setSearchResults(data.results);
+  useEffect(() => {
+  if (!searchQuery.trim()) {
+    setSearchResults([]);
+    return;
   }
+
+  const delay = setTimeout(async () => {
+    const data = await searchMovies(searchQuery);
+    setSearchResults(data.results || []);
+  }, 400);
+
+  return () => clearTimeout(delay);
+}, [searchQuery]);
+
+  const displayedMovies = movies.filter((movie) => movie.status === activeTab);
 
   return (
     <div className="min-h-screen p-6 bg-gradient-to-br from-black via-purple-950 to-black text-white">
 
-      <h1 className="text-3xl font-bold mb-6">My Movies</h1>
+      <div className="flex justify-between items-center mb-6">
+        <h1 className="text-3xl font-bold">My Movies</h1>
 
-      <form onSubmit={handleSearch} className="flex gap-2 mb-6">
-        <input
-          className="border border-white/20 bg-white/10 text-white p-2 rounded w-full"
-          placeholder="Search movie..."
-          value={searchQuery}
-          onChange={(e) => setSearchQuery(e.target.value)}
-        />
-        <button className="bg-purple-600 px-4 rounded">
-          Search
+        <button
+          onClick={handleLogout}
+          className="p-2 rounded-full bg-white/10 border border-white/20 hover:bg-red-500/20 hover:border-red-400 transition"
+          title="Logout"
+        >
+          <LogOut size={18} />
         </button>
-      </form>
-
-      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-5 lg:grid-cols-7 gap-3 mb-10">
-
-        {searchResults.map((movie) => (
-          <div
-            key={movie.id}
-            className="bg-white/10 border border-white/10 rounded-md overflow-hidden"
-          >
-            <img
-              src={
-                movie.poster_path
-                  ? `https://image.tmdb.org/t/p/w342${movie.poster_path}`
-                  : "https://via.placeholder.com/342x513"
-              }
-              alt={movie.title}
-              className="w-full object-cover"
-            />
-
-            <div className="p-1 text-xs font-semibold text-center truncate">
-              {movie.title}
-            </div>
-            {/*b*/}
-            <button
-              onClick={async () => {
-                await addMovie({
-                  tmdbMovieId: movie.id,
-                  title: movie.title,
-                  posterPath: movie.poster_path,
-                  status: "watchlist",
-                  isFavorite: false,
-                });
-
-                loadMovies();
-              }}
-              className="w-full bg-green-600 text-[10px] py-1"
-            >
-              Add
-            </button>
-          </div>
-        ))}
-
       </div>
 
+      <div className="mb-6">
+        <input
+          onFocus={() => setIsSearchOpen(true)}
+          placeholder="Search movie..."
+          className="border border-white/20 bg-white/10 text-white p-2 rounded w-full cursor-pointer"
+          readOnly
+        />
+      </div>
+
+      {isSearchOpen && (
+        <div
+          className="fixed inset-0 bg-black/90 backdrop-blur-md z-50 p-6 overflow-y-auto"
+          onClick={() => setIsSearchOpen(false)}
+        >
+          <div
+            className="w-full min-h-full p-6 "
+            onClick={(e) => e.stopPropagation()}
+          >
+
+            <button
+              onClick={() => setIsSearchOpen(false)}
+              className="text-white mb-4"
+            >
+              ✕ Close
+            </button>
+
+            <input
+              autoFocus
+              value={searchQuery}
+              onChange={(e) => setSearchQuery(e.target.value)}
+              placeholder="Search movie..."
+              className="w-full p-3 rounded bg-white/10 text-white border border-white/20 mb-6"
+            />
+
+            <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-5 lg:grid-cols-7 gap-3">
+
+              {searchResults.map((movie) => (
+                <div
+                  key={movie.id}
+                  className="bg-white/10 rounded overflow-hidden"
+                >
+                  <img
+                    src={`https://image.tmdb.org/t/p/w342${movie.poster_path}`}
+                    className="w-full"
+                  />
+
+                  <div className="text-xs text-center p-1 truncate">
+                    {movie.title}
+                  </div>
+
+                  <div className="flex gap-2 p-2">
+
+                    <button
+                      title="Mark as Watched"
+                      onClick={async () => {
+                        await addMovie({
+                          tmdbMovieId: movie.id,
+                          title: movie.title,
+                          posterPath: movie.poster_path,
+                          status: "watched",
+                          isFavorite: false,
+                        });
+
+                        loadMovies();
+                      }}
+                      className="flex-1 flex justify-center items-center p-2 rounded bg-white/10 hover:bg-blue-500/30 transition"
+                    >
+                      <Eye size={18} className="text-blue-400" />
+                    </button>
+
+                    <button
+                      title="Add to Watchlist"
+                      onClick={async () => {
+                        await addMovie({
+                          tmdbMovieId: movie.id,
+                          title: movie.title,
+                          posterPath: movie.poster_path,
+                          status: "watchlist",
+                          isFavorite: false,
+                        });
+
+                        loadMovies();
+                      }}
+                      className="flex-1 flex justify-center items-center p-2 rounded bg-white/10 hover:bg-purple-500/30 transition"
+                    >
+                      <Bookmark size={18} className="text-purple-300" />
+                    </button>
+
+                  </div>
+                </div>
+              ))}
+
+          </div>
+        </div>
+      </div>
+      )}
+      <div className="flex gap-3 mb-6">
+
+      <button
+        onClick={() => setActiveTab("watched")}
+        className={`px-5 py-2 rounded-full border transition ${
+          activeTab === "watched"
+            ? "bg-purple-600 border-purple-600 text-white"
+            : "bg-white/10 border-white/20 text-gray-300 hover:bg-white/20"
+        }`}
+      >
+        Watched
+      </button>
+
+      <button
+        onClick={() => setActiveTab("watchlist")}
+        className={`px-5 py-2 rounded-full border transition ${
+          activeTab === "watchlist"
+            ? "bg-purple-600 border-purple-600 text-white"
+            : "bg-white/10 border-white/20 text-gray-300 hover:bg-white/20"
+        }`}
+      >
+        Watchlist
+      </button>
+
+    </div>
       <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-5 lg:grid-cols-7 gap-3">
 
-        {movies.map((movie) => (
+        {displayedMovies.map((movie) => (
           <div
             key={movie.id}
             className="bg-white/10 border border-white/10 rounded-md overflow-hidden flex flex-col"
@@ -140,32 +233,34 @@ export default function Movies() {
                 <Eye size={iconSize} />
               </button>
 
-              <button
-                title={
+              {movie.status === "watched" && (
+                <button
+                  title={
                     movie.isFavorite
-                    ? "Remove from Favorites"
-                    : "Add to Favorites"
-                }
-                onClick={async () => {
+                      ? "Remove from Favorites"
+                      : "Add to Favorites"
+                  }
+                  onClick={async () => {
                     await updateMovie(movie.id, {
-                    status: movie.status,
-                    isFavorite: !movie.isFavorite,
+                      status: movie.status,
+                      isFavorite: !movie.isFavorite,
                     });
 
                     setMovies(await getMovies());
-                }}
-                className="flex-1 flex justify-center items-center transition"
+                  }}
+                  className="flex-1 flex justify-center"
                 >
-                <Heart
+                  <Heart
                     size={iconSize}
                     fill={movie.isFavorite ? "currentColor" : "none"}
                     className={
-                    movie.isFavorite
+                      movie.isFavorite
                         ? "text-red-500"
                         : "text-gray-400 hover:text-red-400"
                     }
-                />
+                  />
                 </button>
+              )}
 
               <button
                 title="Delete"
